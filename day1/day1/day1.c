ba96526875c379f68fe43f46ec201c307b791a7c
SpicyJeokbal <155035421+SpicyJeokbal@users.noreply.github.com>
2025-04-27 18:57:14 +0800

    Add files via upload

---
+++ day1/day1/day1.c
@@ -0,0 +1,12 @@
+//day1 of learning c
+//name: Leigh
+//date: 27/04/2025
+
+//setup environment/compiler and print out hello world
+
+#include <stdio.h>
+
+int main(){
+    printf("hello world"); //only takes string unless you provide a format
+    return 0;
+}
