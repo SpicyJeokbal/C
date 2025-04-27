333ed244b5cdb3d00d0f7a6b614bd4a95f33da8f
SpicyJeokbal <155035421+SpicyJeokbal@users.noreply.github.com>
2025-04-27 19:27:09 +0800

    Add files via upload

---
+++ day6/day6.c
@@ -0,0 +1,21 @@
+//day6 of learning c
+//name: Leigh
+//date: 27/04/2025
+
+//Multiply 2 numbers, print the result
+
+#include <stdio.h>
+
+int main(){
+
+    int x = 10;
+    int y = 10;
+
+    printf("%d \n", x * y);
+
+    float a = 9.81;
+    float b = 10.01;
+
+    printf("%.4f", a * b);
+
+}
